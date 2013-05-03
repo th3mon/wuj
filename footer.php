df6d8b092980cc4ad96b0bfce2cb4afe2e7fe577
th3mon <p.szelenberger@gmail.com>
2013-05-03 21:46:12 +0200

    Pierwszy komit

---
+++ footer.php
@@ -0,0 +1,27 @@
+			<footer class="footer" role="contentinfo">
+			
+				<div id="inner-footer" class="wrap clearfix">
+					<div id="partnerzy" class="partnerzy">
+						<h2>partnerzy:</h2>
+						<div class="mock"></div>
+						<?php logo_slider(); ?>
+					</div>
+					
+					<nav role="navigation">
+    					<?php bones_footer_links(); ?>
+	                </nav>
+	                		
+					<p class="source-org copyright">&copy; Fundacja Studentów i Absolwentów Uniwersytetu Jagiellońskiego "Bratniak" 2011.</p>
+
+				</div> <!-- end #inner-footer -->
+				
+			</footer> <!-- end footer -->
+		
+		</div> <!-- end #container -->
+		
+		<!-- all js scripts are loaded in library/bones.php -->
+		<?php wp_footer(); ?>
+
+	</body>
+
+</html> <!-- end page. what a ride! -->
